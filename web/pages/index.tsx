2fe6e0798465ec22a7345301840eb39f35668d62
johntheo <john.theo.souza@gmail.com>
2022-11-01 18:12:15 -0300

    Finishing Front-end setup

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
-  return (
-    <h1>Hello John</h1>
-  )
+interface HomeProps{
+  count: number;
 }
+
+export default function Home(props: HomeProps) {
+  return <h1>Contagem: {props.count} </h1>;
+}
+
+//This code is ran in the WebServer so this isnt affected when a crwaler or Google access the page with "Debuger > Javascript Disable".
+//This is SSR. The application will be indexed
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:3333/pools/count");
+  const data = await response.json();
+  console.log(data);
+
+  return {
+    props: {
+      count: data.count,
+    },
+  };
+};
