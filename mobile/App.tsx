860d707c948ba96e9633e5be2086260e113a2de0
johntheo <john.theo.souza@gmail.com>
2022-11-02 00:29:06 -0300

    creating components

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,15 +1,27 @@
 import { StatusBar } from "expo-status-bar";
 import { NativeBaseProvider, Text, Center } from "native-base";
-import {} from './src/styles/theme'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { SignIn } from "./screens/SignIn";
+import { Loading } from "./components/Loading";
+import { THEME } from "./src/styles/theme";
 
 export default function App() {
+  //Using fontsLoaded to ensure the font is loaded
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
   return (
     //This will enure that all the application will be able to use Native Base components
-    <NativeBaseProvider> 
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24} >Hello React Native!</Text>
-        <StatusBar style="auto" />
-      </Center>
+    <NativeBaseProvider theme={THEME}>
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
--- mobile/README.md
+++ mobile/README.md
@@ -1,7 +1,12 @@
 - Resource of components: https://nativebase.io/ 
 - Due new npm lts and an yet unsupportive expo, it was needed to add this to `package.json`
 ```
-"overrides": {
-    "react": "18.0.0"
-  },
+    "overrides": {
+        "react": "18.0.0"
+    },
 ```
+- Installing fonts
+```
+    npx expo install expo-font @expo-google-fonts/roboto
+```
+- 
