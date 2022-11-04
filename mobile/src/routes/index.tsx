379ce9ac7d4f18de672d6dac18f28c5d326f0e3a
johntheo <john.theo.souza@gmail.com>
2022-11-04 11:03:43 -0300

    Redirecting after login

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -2,14 +2,20 @@
 //This code also is responsible to create the logic for choosing between the routes for logged in and logged out users
 
 import { NavigationContainer } from "@react-navigation/native";
+import { Box } from "native-base";
+import { useAuth } from "../hooks/useAuth";
 import { SignIn } from "../screens/SignIn";
 import { AppRoutes } from "./app.routes";
 
 export function Routes() {
+  const { user } = useAuth();
+
   return (
-    <NavigationContainer>
-      {/* <AppRoutes /> */}
-      <SignIn/>
-    </NavigationContainer>
+    //This box is just to prevent some glitches when changing routes
+    <Box flex={1} bg="gray.900">
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SignIn />}
+      </NavigationContainer>
+    </Box>
   );
 }
