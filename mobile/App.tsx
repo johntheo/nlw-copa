1d9438af8c3e305995640ff55f8fd2812de3d12f
johntheo <john.theo.souza@gmail.com>
2022-11-04 09:21:35 -0300

    Basic Navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -8,7 +8,7 @@ import {
 import { Loading } from "./src/components/Loading";
 import { THEME } from "./src/styles/theme";
 import { AuthContextProvider } from "./src/contexts/AuthContext";
-import { Pools } from "./src/screens/Pools";
+import { Routes } from "./src/routes";
 
 export default function App() {
   //Using fontsLoaded to ensure the font is loaded
@@ -27,7 +27,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <Pools /> : <Loading />}
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
---
+++ mobile/src/routes/index.tsx
@@ -0,0 +1,13 @@
+//Using this index file will enable us to just import Routes later
+//This code also is responsible to create the logic for choosing between the routes for logged in and logged out users
+
+import { NavigationContainer } from "@react-navigation/native";
+import { AppRoutes } from "./app.routes";
+
+export function Routes() {
+  return (
+    <NavigationContainer>
+      <AppRoutes />
+    </NavigationContainer>
+  );
+}
