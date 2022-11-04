3ecbb8e698efd65e6f8071d13fb7fcf2e6b9eb3e
johntheo <john.theo.souza@gmail.com>
2022-11-04 10:51:21 -0300

    Authentication as storing user info

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -2,12 +2,14 @@
 //This code also is responsible to create the logic for choosing between the routes for logged in and logged out users
 
 import { NavigationContainer } from "@react-navigation/native";
+import { SignIn } from "../screens/SignIn";
 import { AppRoutes } from "./app.routes";
 
 export function Routes() {
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {/* <AppRoutes /> */}
+      <SignIn/>
     </NavigationContainer>
   );
 }
