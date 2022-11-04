9be441e99f60c2fba665b76c8abc7b25bf225b0d
johntheo <john.theo.souza@gmail.com>
2022-11-04 09:53:51 -0300

    Tabs navigation

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -2,8 +2,10 @@ import { VStack, Icon } from "native-base";
 import { Octicons } from "@expo/vector-icons";
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 export function Polls() {
+  const {navigate} = useNavigation()
   return (
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
@@ -20,6 +22,7 @@ export function Polls() {
           leftIcon={
             <Icon as={Octicons} name="search" color="black" size="md" />
           }
+          onPress={() => navigate("find")}
         />
       </VStack>
     </VStack>
